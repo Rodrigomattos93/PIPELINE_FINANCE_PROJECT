4cec64219d1f4acdf635880771a7e8eeb5b0c7ec
Rodrigo Mattos <rodrigo.mattos.rocha@poli.ufrj.br>
2025-06-06 20:18:57 -0300

    update

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,2 @@
 FROM astrocrpublic.azurecr.io/runtime:3.0-2
+RUN pip install dbt-postgres
--- dags/main.py
+++ dags/main.py
@@ -1,17 +1,22 @@
-import os
 from datetime import datetime
 
 from airflow.decorators import dag, task
-from extract.bacen.extract_ipca_rate import extract_inflation_from_bacen_api
-from extract.ibge.extract_unemployment_rate import \
+from airflow.operators.bash import BashOperator
+
+from include.extract.bacen.extract_ipca_rate import \
+    extract_inflation_from_bacen_api
+from include.extract.ibge.extract_unemployment_rate import \
     extract_unemployment_from_ibge_api
+
+DBT_PROJECT_DIR = "/usr/local/airflow/pipeline_project_2"
 
 
 @dag(
     start_date=datetime(2025, 1, 1),
-    schedule="*/1 * * * *",
+    schedule=None,
     catchup=False,
     description="Pipeline that extracts inflation and unemployment data",
+    is_paused_upon_creation=True,
 )
 def pipeline():
 
@@ -23,16 +28,13 @@ def pipeline():
     def extract_unemployment_task():
         extract_unemployment_from_ibge_api(2020)
 
-    @task
-    def run_dbt_task():
-        os.chdir(os.path.join(os.path.dirname(__file__), "..", "pipeline_project_2"))
-        exit_code = os.system("dbt run")
-        if exit_code != 0:
-            raise Exception("dbt run failed")
+    run_dbt = BashOperator(
+        task_id="run_dbt", bash_command=f"dbt run --project-dir {DBT_PROJECT_DIR}"
+    )
 
     t1 = extract_inflation_task()
     t2 = extract_unemployment_task()
-    t3 = run_dbt_task()
+    t3 = run_dbt
 
     [t1, t2] >> t3
 
